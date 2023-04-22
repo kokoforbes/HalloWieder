c9d25dd76a30dcd034eaf4bba920ca03794f0bec
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-04-22 08:19:01 +0100

    Components clean up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
 <template>
-  <div class="bg-black"></div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="app">HEllo</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
